f62ad6afa8d1640f98ad7b22d663a34fde95152c
maronda <marondaanderson@gmail.com>
2021-05-24 11:20:33 -0400

    Initial

---
+++ gittest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace gittest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Maronda.");
+            Console.WriteLine("Today is day 1 of my C# Bootcamp.");
+        }
+    }
+}
